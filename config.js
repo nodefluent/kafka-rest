c13218c366be39ad6257f5b77cf3c434ca059a37
yacut <roman.rogozhnikov@gmail.com>
2017-09-10 11:22:40 +0200

    better config map for docker env

--- config.js
+++ config.js
@@ -39,49 +39,15 @@ const config = {
   }
 };
 
-if(process.env.KAFKA_REST_METADATA_BROKER_LIST){
-  config.producer.noptions["metadata.broker.list"] = process.env.KAFKA_REST_METADATA_BROKER_LIST;
-  config.consumer.noptions["metadata.broker.list"] = process.env.KAFKA_REST_METADATA_BROKER_LIST;
-}
-if(process.env.KAFKA_REST_SECURITY_PROTOCOL){
-  config.producer.noptions["security.protocol"] = process.env.KAFKA_REST_SECURITY_PROTOCOL;
-  config.consumer.noptions["security.protocol"] = process.env.KAFKA_REST_SECURITY_PROTOCOL;
-}
-if(process.env.KAFKA_REST_SSL_KEY_LOCATION){
-  config.producer.noptions["ssl.key.location"] = process.env.KAFKA_REST_SSL_KEY_LOCATION;
-  config.consumer.noptions["ssl.key.location"] = process.env.KAFKA_REST_SSL_KEY_LOCATION;
-}
-if(process.env.KAFKA_REST_SSL_KEY_PASSWORD){
-  config.producer.noptions["ssl.key.password"] = process.env.KAFKA_REST_SSL_KEY_PASSWORD;
-  config.consumer.noptions["ssl.key.password"] = process.env.KAFKA_REST_SSL_KEY_PASSWORD;
-}
-if(process.env.KAFKA_REST_SSL_CERTIFICATE_LOCATION){
-  config.producer.noptions["ssl.certificate.location"] = process.env.KAFKA_REST_SSL_CERTIFICATE_LOCATION;
-  config.consumer.noptions["ssl.certificate.location"] = process.env.KAFKA_REST_SSL_CERTIFICATE_LOCATION;
-}
-if(process.env.KAFKA_REST_SSL_CA_LOCATION){
-  config.producer.noptions["ssl.ca.location"] = process.env.KAFKA_REST_SSL_CA_LOCATION;
-  config.consumer.noptions["ssl.ca.location"] = process.env.KAFKA_REST_SSL_CA_LOCATION;
-}
-if(process.env.KAFKA_REST_SASL_MECHANISMS){
-  config.producer.noptions["sasl.mechanisms"] = process.env.KAFKA_REST_SASL_MECHANISMS;
-  config.consumer.noptions["sasl.mechanisms"] = process.env.KAFKA_REST_SASL_MECHANISMS;
-}
-if(process.env.KAFKA_REST_SASL_USERNAME){
-  config.producer.noptions["sasl.username"] = process.env.KAFKA_REST_SASL_USERNAME;
-  config.consumer.noptions["sasl.username"] = process.env.KAFKA_REST_SASL_USERNAME;
-}
-if(process.env.KAFKA_REST_SASL_PASSWORD){
-  config.producer.noptions["sasl.password"] = process.env.KAFKA_REST_SASL_PASSWORD;
-  config.consumer.noptions["sasl.password"] = process.env.KAFKA_REST_SASL_PASSWORD;
-}
-if(process.env.KAFKA_REST_API_VERSION_REQUEST){
-  config.producer.noptions["api.version.request"] = process.env.KAFKA_REST_API_VERSION_REQUEST;
-  config.consumer.noptions["api.version.request"] = process.env.KAFKA_REST_API_VERSION_REQUEST;
-}
-if(process.env.KAFKA_REST_DEBUG){
-  config.producer.noptions["debug"] = process.env.KAFKA_REST_DEBUG;
-  config.consumer.noptions["debug"] = process.env.KAFKA_REST_DEBUG;
-}
+Object.keys(process.env).forEach((env)=>{
+  if(env.startsWith("KAFKA_REST_CONSUMER_")){
+    config.consumer.noptions[env.slice(20).toLowerCase().replace(/_/g, ".")] = process.env[env];
+  } else if(env.startsWith("KAFKA_REST_PRODUCER_")){
+    config.producer.noptions[env.slice(20).toLowerCase().replace(/_/g, ".")] = process.env[env];
+  } else if(env.startsWith("KAFKA_REST_")){
+    config.consumer.noptions[env.slice(11).toLowerCase().replace(/_/g, ".")] = process.env[env];
+    config.producer.noptions[env.slice(11).toLowerCase().replace(/_/g, ".")] = process.env[env];
+  }
+});
 
 module.exports= config;
