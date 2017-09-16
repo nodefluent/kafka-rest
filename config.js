540aa5890828f0891617d7355a3344e73a60efc3
yacut <roman.rogozhnikov@gmail.com>
2017-09-16 18:48:47 +0200

    fix config for zookeeper

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ const logger = {
 };
 
 const host = "kafka";
+const zhost = "zookeeper";
 
 const config = {
   logger,
@@ -40,7 +41,7 @@ const config = {
     port: 8082
   },
   zookeeper: {
-    host: "localhost:2181",
+    host: `${zhost}:2181`,
     timeout: 20000
   }
 };
@@ -52,6 +53,8 @@ Object.keys(process.env).forEach((env)=>{
     config.consumer.noptions[env.slice(20).toLowerCase().replace(/_/g, ".")] = process.env[env];
   } else if(env.startsWith("KAFKA_REST_PRODUCER_")){
     config.producer.noptions[env.slice(20).toLowerCase().replace(/_/g, ".")] = process.env[env];
+  } else if(env.startsWith("KAFKA_REST_ZOOKEEPER_")){
+    config.zookeeper[env.slice(21).toLowerCase().replace(/_/g, ".")] = process.env[env];
   } else if(env.startsWith("KAFKA_REST_") && !env.startsWith("KAFKA_REST_UI_")){
     config.consumer.noptions[env.slice(11).toLowerCase().replace(/_/g, ".")] = process.env[env];
     config.producer.noptions[env.slice(11).toLowerCase().replace(/_/g, ".")] = process.env[env];
